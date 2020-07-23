8a52f66a37676c2c47d60eb1aee1f8d8a3a18337
Victor Lopes Fernandes <41849718+VictorLopes3101@users.noreply.github.com>
2020-07-23 00:03:07 -0300

    update files

--- NLW/scripts/create-point.js
+++ NLW/scripts/create-point.js
@@ -1,4 +1,3 @@
-
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
@@ -22,14 +21,18 @@ function getCities(event) {
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text
 
-
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
     fetch(url)
         .then(res => res.json())
         .then(cities => {
+            
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
+
             citySelect.disabled = false
         })
 }
@@ -37,3 +40,39 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+// Itens de colete
+
+ const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+ for (const item of itemsToCollect) {
+     item.addEventListener("click", handleSelectedItem)
+ }
+
+let selectedItems = []
+
+ function handleSelectedItem(event) {  
+
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId
+        return itemFound
+    })
+
+   if(alreadySelected >= 0) {
+       const filteredItems = selectedItems.filter(item => {
+           const itemIsDifferente = item != itemId
+           return itemIsDifferente 
+        
+       })
+
+       selectedItems = filteredItems
+   } else {
+       selectedItems.push(itemId)
+       console.log(selectedItems)
+   }
+ }
